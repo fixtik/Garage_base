e7c7bc72370532cb08718432335ae9f82212cb5c
NikitaEliz <nikita_eliz@mail.ru>
2023-10-10 17:30:06 +0300

    added floatValidator and showHelp form

--- ui/validators.py
+++ ui/validators.py
@@ -1,10 +1,14 @@
 from PySide6 import QtCore, QtGui
 
+def showHelp(window):
+    button = window
+    button.setToolTip("Формат: *.*")
+    button.setToolTipDuration(3000)
 
-
-
-
-
+def floatValidator():
+    reg_ex = QtCore.QRegularExpression()
+    reg_ex.setPattern('[0-9]+[,.]?[0-9]+')
+    return QtGui.QRegularExpressionValidator(reg_ex)
 
 def onlyNumValidator():
     """валидатор для ввода только цифр"""
