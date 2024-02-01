4fd5b6a03a341bf0969ba4e3564123a71741f7d5
Nikitka <nikita_eliz@mail.ru>
2024-02-01 12:41:25 +0300

    обновлено

--- ui/vigruzki_functions.py
+++ ui/vigruzki_functions.py
@@ -3,9 +3,27 @@ from datetime import datetime
 import os
 import shutil
 import constants
+import pandas as pd
+import numpy as np
 
 
-# import pandas
+def set_topik():
+    topik = ('№ п/п', 'Доходная часть сметы', 'Сумма доходов Смета', 'Сумма доходов исполнение Сметы',
+             'Расхождение', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь',
+             'октябрь', 'ноябрь', 'декабрь')
+    print(pd.Series(topik))
+    # topik.to_excel("docs/smeta.xlsx")
+
+
+def smeta_action():
+    if not os.path.isdir(constants.DEFAULT_DOCS_DIR_PASS):  # Проверяем создана директория или нет.
+        os.makedirs(constants.DEFAULT_DOCS_DIR_PASS, mode=0o777)  # Создаем директорию.
+    # smeta_file = open("docs/smeta.xlsx", "w+")
+    # smeta_file.to_excel("docs/smeta.xlsx")
+    # smeta_file.write("Привет, файл!")
+    # smeta_file.close()
+    set_topik()
+    print("suka")
 
 
 def spisok_action():
@@ -13,9 +31,3 @@ def spisok_action():
         os.makedirs(constants.DEFAULT_DOCS_DIR_PASS, mode=0o777)  # Создаем директорию.
 
     print('bomj')
-
-
-def smeta_action():
-    if not os.path.isdir(constants.DEFAULT_DOCS_DIR_PASS):  # Проверяем создана директория или нет.
-        os.makedirs(constants.DEFAULT_DOCS_DIR_PASS, mode=0o777)  # Создаем директорию.
-    print("suka")
