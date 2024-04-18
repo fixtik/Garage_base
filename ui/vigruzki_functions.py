04cfd441609ac558083b9e522b94934560cfc959
Nikitka <nikita_eliz@mail.ru>
2024-04-18 17:56:31 +0300

    добавлено создание сметы

--- ui/vigruzki_functions.py
+++ ui/vigruzki_functions.py
@@ -1,12 +1,12 @@
+import os
 from dataclasses import dataclass
+from openpyxl import Workbook
+from datetime import datetime
 
-
-def set_topik():
-    topik = ('№ п/п', 'Доходная часть сметы', 'Сумма доходов Смета', 'Сумма доходов исполнение Сметы',
-             'Расхождение', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь',
-             'октябрь', 'ноябрь', 'декабрь')
-    print(pd.Series(topik))
-    # topik.to_excel("docs/smeta.xlsx")
+import constants
+import sqlite_qwer
+import datetime
+import calendar
 
 
 class Smeta():
@@ -14,31 +14,75 @@ class Smeta():
         self.db = db  # БД
 
     def smeta_action(self):
+        # Проверили наличие директории для файла сметы
         if not os.path.isdir(constants.DEFAULT_DOCS_DIR_PASS):  # Проверяем создана директория или нет.
             os.makedirs(constants.DEFAULT_DOCS_DIR_PASS, mode=0o777)  # Создаем директорию.
-        print('2')
-        if self.db.execute(sqlite_qwer.sql_select_all_from_table(constants.CONTRIB_TABLE)):
-            conribs = self.db.cursor.fetchall()
-            for conrib in conribs:
-                print(conrib)
-                # con = ui.contribute_functions.Contribution_lite(*conrib)
-                # 'SELECT * FROM contribution WHERE date(pay_date, 'start of year') = date(datetime('now'), 'start of year');'
+        wb = Workbook()  # создаем книгу
+        ws = wb.active  # делаем единственный лист активным
+        ws.title = "Смета"  # меняем название листа
+        topik = ['№ п/п', 'Доходная часть сметы', 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль',
+                 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+        ws.append(topik)
+        dict = {
+            'id': '',
+            'name': '',  # название платежа
+            'January': '',
+            'February': '',
+            'March': '',
+            'April': '',
+            'May': '',
+            'June': '',
+            'July': '',
+            'August': '',
+            'September': '',
+            'October': '',
+            'November': '',
+            'December': ''
+        }
+        id_name = 0  # номер по порядку
+        for i in range(2):  # пробегаем blance_count (влияет/не влияет платеж на баланс гаража)
+            if i == 0:
+                ws.append(['Платежи не учитываемые в балансе'])
+            if self.db.execute(sqlite_qwer.smeta_reqest(balance_count=i)):
+                contribs = self.db.cursor.fetchall()
+                for contrib in contribs:  # пробегаем по платежам
+                    con = SmetaStructureLite(*contrib)
+                    name = con.name + (" (Наличный расчет)" if con.pay_kind == '1' else " (Безналичный расчет)")
+                    dict['id'] = id_name  # записываем номер по порядку
+                    if dict['name'] != name:  # проверяем не закончились ли платежи с одним названием
+                        if id_name != 0:  # пропускаем цикл чтобы не записывать пустую строку
+                            ws.append(list(dict.values()))  # записываем данные по платежу
+                        id_name += 1  # номер по порядку
+                        dict.update({}.fromkeys(dict, 0))  # очищаем словарь
+                        if i == 1:
+                            ws.append(['Платежи учитываемые в балансе'])
+                            i += 1  # плюсуем чтобы больще не записывалось
+                        dict['name'] = name  # записываем в словарь название платежа
+                        if calendar.month_name[int(con.month)] in dict:  # Проверяем есть ли название месяца в словаре
+                            # Записываем сумму платежа в нужный месяц
+                            dict[calendar.month_name[int(con.month)]] = con.TotalSum
+                    else:
+                        if calendar.month_name[int(con.month)] in dict:  # Проверяем есть ли название месяца в словаре
+                            # Записываем сумму платежа в нужный месяц
+                            dict[calendar.month_name[int(con.month)]] = con.TotalSum
+        file_name = f'{constants.DEFAULT_DOCS_DIR_PASS}Смета_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")}.xlsx'
+
+        print(file_name)
+        wb.save(file_name)  # сохраняем эксель
+        for row in ws.iter_rows(values_only=True):
+            print(row)
+
+
+@dataclass
+class SmetaStructureLite:
+    """Класс для работы с данными сметы"""
+    name: str = ''  # название платежа
+    month: str = ''  # месяц в котором был совершен платеж
+    pay_kind: str = ''  # нал(1) / безнал(2)
+    TotalSum: str = ''  # итоговая сумма платежей за месяц
 
 
 def spisok_action():
     if not os.path.isdir(constants.DEFAULT_DOCS_DIR_PASS):  # Проверяем создана директория или нет.
         os.makedirs(constants.DEFAULT_DOCS_DIR_PASS, mode=0o777)  # Создаем директорию.
     print('bomj')
-
-
-@dataclass
-class SmetaStructure:
-    """Класс для работы с данными сметы"""
-    id: int = ''
-    size_id: str = ''  # вид типоразмера
-    width: str = ''  # размеры
-    length: str = ''
-    height: str = ''
-    year: str = ''  # год за который платят1
-    value: str = ''  # сумма взноса
-    bilingDate: str = ''  # дата выставления счета
